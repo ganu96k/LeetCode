83c08705a6eb0279ed9f197dfd57f24f67dade6c
ganu96k <99240676+ganu96k@users.noreply.github.com>
2026-09-07 15:08:10 +0530

    Time: 49 ms (25.10%) | Memory: 52.4 MB (98.24%) - LeetSync

--- 2864-is-object-empty/is-object-empty.js
+++ 2864-is-object-empty/is-object-empty.js
@@ -3,6 +3,8 @@
  * @return {boolean}
  */
 var isEmpty = function(obj) {
-    
-     return Object.keys(obj).length === 0;
+
+var res = Object.keys(obj);
+  return res.length ? false : true ;    
+    // return Object.keys(obj).length === 0;
 };
